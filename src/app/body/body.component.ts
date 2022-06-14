137e00b128e2e188ad80a6f6e4688fb4b7ebfbf4
Nick Bardier <bardier.pro@gmail.com>
2022-06-15 02:12:21 +0300

    мелкие правки

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -12,7 +12,12 @@ export class BodyComponent implements OnInit {
 
   constructor(public currencyService: CurrencyService) {}
 
-  onFirstIn(inputValue: string, firstSel: string, secondSel: string) {
+  onInput(
+    inputValue: string,
+    firstSel: string,
+    secondSel: string,
+    flag: boolean
+  ) {
     this.firstInput = inputValue.replace(/[^0-9\.]/g, '');
     if (!inputValue) {
       return;
@@ -27,63 +32,54 @@ export class BodyComponent implements OnInit {
       if (el.cc === secondSel) y = el.rate;
     });
 
-    switch (firstSel) {
-      case 'UAH':
-        if (secondSel === 'UAH') res = inputValue;
-        else res = parseFloat(inputValue) / y + '';
-        break;
-      case 'EUR':
-        if (secondSel === 'EUR') res = inputValue;
-        else if (secondSel === 'UAH') res = parseFloat(inputValue) * x + '';
-        else if (secondSel === 'USD')
-          res = (x / y) * parseFloat(inputValue) + '';
-        break;
-      case 'USD':
-        if (secondSel === 'USD') res = inputValue;
-        else if (secondSel === 'UAH') res = parseFloat(inputValue) * x + '';
-        else if (secondSel === 'EUR')
-          res = (x / y) * parseFloat(inputValue) + '';
-        break;
-    }
+    if (flag) {
+      switch (firstSel) {
+        case 'UAH':
+          if (secondSel === 'UAH') res = inputValue;
+          else res = parseFloat(inputValue) / y + '';
+          break;
+        case 'EUR':
+          if (secondSel === 'EUR') res = inputValue;
+          else if (secondSel === 'UAH') res = parseFloat(inputValue) * x + '';
+          else if (secondSel === 'USD')
+            res = (x / y) * parseFloat(inputValue) + '';
+          break;
+        case 'USD':
+          if (secondSel === 'USD') res = inputValue;
+          else if (secondSel === 'UAH') res = parseFloat(inputValue) * x + '';
+          else if (secondSel === 'EUR')
+            res = (x / y) * parseFloat(inputValue) + '';
+          break;
+      }
 
-    this.secondInput = isNaN(parseFloat(res)) ? '' : parseFloat(res).toFixed(2);
+      this.secondInput = isNaN(parseFloat(res))
+        ? ''
+        : parseFloat(res).toFixed(2);
+    } else {
+      switch (secondSel) {
+        case 'UAH':
+          if (firstSel === 'UAH') res = inputValue;
+          else res = parseFloat(inputValue) / x + '';
+          break;
+        case 'EUR':
+          if (firstSel === 'EUR') res = inputValue;
+          else if (firstSel === 'UAH') res = parseFloat(inputValue) * y + '';
+          else if (firstSel === 'USD')
+            res = (y / x) * parseFloat(inputValue) + '';
+          break;
+        case 'USD':
+          if (firstSel === 'USD') res = inputValue;
+          else if (firstSel === 'UAH') res = parseFloat(inputValue) * y + '';
+          else if (firstSel === 'EUR')
+            res = (y / x) * parseFloat(inputValue) + '';
+          break;
+      }
+
+      this.firstInput = isNaN(parseFloat(res))
+        ? ''
+        : parseFloat(res).toFixed(2);
+    }
   }
 
-  onSecondIn(inputValue: string, firstSel: string, secondSel: string) {
-    this.secondInput = inputValue.replace(/[^0-9\.]/g, '');
-    if (!inputValue) {
-      return;
-    }
-
-    let x = 0;
-    let y = 0;
-    let res = '';
-
-    this.currencyService.allCurrencies.forEach((el) => {
-      if (el.cc === firstSel) x = el.rate;
-      if (el.cc === secondSel) y = el.rate;
-    });
-
-    switch (secondSel) {
-      case 'UAH':
-        if (firstSel === 'UAH') res = inputValue;
-        else res = parseFloat(inputValue) / x + '';
-        break;
-      case 'EUR':
-        if (firstSel === 'EUR') res = inputValue;
-        else if (firstSel === 'UAH') res = parseFloat(inputValue) * y + '';
-        else if (firstSel === 'USD')
-          res = (y / x) * parseFloat(inputValue) + '';
-        break;
-      case 'USD':
-        if (firstSel === 'USD') res = inputValue;
-        else if (firstSel === 'UAH') res = parseFloat(inputValue) * y + '';
-        else if (firstSel === 'EUR')
-          res = (y / x) * parseFloat(inputValue) + '';
-        break;
-    }
-
-    this.firstInput = isNaN(parseFloat(res)) ? '' : parseFloat(res).toFixed(2);
-  }
   ngOnInit(): void {}
 }
